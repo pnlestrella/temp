40ba32ae61d19c8748efaca535ff451547b7edee
pnestrella <158650319+pnestrella@users.noreply.github.com>
2025-08-14 17:33:11 +0800

    a

--- app.config.js
+++ app.config.js
@@ -6,14 +6,18 @@ export default {
     slug: "connext",
     version: "1.0.0",
 
+
     extra: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,  
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
       FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
       FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET, 
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
       FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
       FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+      "eas": {
+        "projectId": "b8b30b60-6247-4bb9-897e-9c3cb298f892"
+      }
     },
 
     web: {
@@ -44,7 +48,8 @@ export default {
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
         backgroundColor: "#ffffff"
-      }
+      },
+      package: "com.patricknhel.myapp"
     }
   }
 }
--- screens/common/RegisterScreenJS.tsx
+++ screens/common/RegisterScreenJS.tsx
@@ -3,8 +3,6 @@ import { useState } from 'react';
 import { Button, Text, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import appLogo from '../../assets/images/'
-
 export const RegisterScreenJS = () => {
   const {registerJSeekers} = useAuth(); 
 
